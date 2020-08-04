fd25935d372b77e00402bf6033be19c6742b131f
Sandeep-korada <69131888+Sandeep-korada@users.noreply.github.com>
2020-08-04 17:30:47 +0530

    Add files via upload

---
+++ Product Inventory System.mm
@@ -0,0 +1,85 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Product Inventory System" FOLDED="false" ID="ID_1299001268" CREATED="1596466725419" MODIFIED="1596467179797"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="9" BOLD="true" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="24"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<node TEXT="Root Component" POSITION="right" ID="ID_36162894" CREATED="1596466804876" MODIFIED="1596469516505">
+<node TEXT="Header Component" ID="ID_778311991" CREATED="1596466844381" MODIFIED="1596467255837" HGAP_QUANTITY="18.499999865889553 pt" VSHIFT_QUANTITY="-36.74999890476469 pt">
+<node TEXT="Navbar Component" ID="ID_1416945659" CREATED="1596467154329" MODIFIED="1596467367387">
+<node TEXT="Add Product Component" ID="ID_38318825" CREATED="1596467373489" MODIFIED="1596467420309">
+<node TEXT="AddPage Component" ID="ID_1893659594" CREATED="1596467466732" MODIFIED="1596467483040"/>
+</node>
+<node TEXT="Dashboard Component" ID="ID_241250579" CREATED="1596467392789" MODIFIED="1596467429909">
+<node TEXT="Graph Component" ID="ID_357315954" CREATED="1596467492957" MODIFIED="1596467505624"/>
+</node>
+<node TEXT="Logo Component" ID="ID_618335544" CREATED="1596467397920" MODIFIED="1596467438669"/>
+<node TEXT="Search Component" ID="ID_1502088408" CREATED="1596467403223" MODIFIED="1596467453426"/>
+<node TEXT="Logout Component" ID="ID_1897911249" CREATED="1596467407409" MODIFIED="1596467463598"/>
+</node>
+</node>
+<node TEXT="Login Component" ID="ID_91056396" CREATED="1596466859127" MODIFIED="1596467295891" HGAP_QUANTITY="22.999999731779106 pt" VSHIFT_QUANTITY="-17.249999485909953 pt"/>
+<node TEXT="SignUp Component" ID="ID_1433836247" CREATED="1596467066315" MODIFIED="1596469490852" HGAP_QUANTITY="24.499999687075626 pt" VSHIFT_QUANTITY="5.249999843537815 pt"/>
+<node TEXT="Content Component" ID="ID_1564037871" CREATED="1596467078775" MODIFIED="1596467279745" HGAP_QUANTITY="18.499999865889553 pt" VSHIFT_QUANTITY="-11.249999664723884 pt">
+<node TEXT="Home Component" ID="ID_792269144" CREATED="1596467511701" MODIFIED="1596467536367">
+<node TEXT="AllProducts Components" ID="ID_1888534486" CREATED="1596467540361" MODIFIED="1596467554565">
+<node TEXT="Product Component" ID="ID_1171386788" CREATED="1596467570936" MODIFIED="1596469712867">
+<node TEXT="Product Image Component" ID="ID_1097173198" CREATED="1596467582928" MODIFIED="1596467969332"/>
+<node TEXT="Product Details component" ID="ID_1079508089" CREATED="1596467630573" MODIFIED="1596467974677">
+<node TEXT="Name" ID="ID_1362195255" CREATED="1596467860560" MODIFIED="1596467946439"/>
+<node TEXT="Price" ID="ID_636805209" CREATED="1596467874362" MODIFIED="1596467952811"/>
+<node TEXT="Description" ID="ID_44293046" CREATED="1596467884651" MODIFIED="1596467961034"/>
+<node TEXT="Rating" ID="ID_799424270" CREATED="1596467909930" MODIFIED="1596467941430"/>
+</node>
+<node TEXT="Update Component" ID="ID_905634389" CREATED="1596467644104" MODIFIED="1596467983051"/>
+<node TEXT="Delete Component" ID="ID_1359689134" CREATED="1596467653330" MODIFIED="1596469712854" VSHIFT_QUANTITY="20.24999939650299 pt"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Footer Component" ID="ID_337695103" CREATED="1596467083410" MODIFIED="1596469516494" HGAP_QUANTITY="19.249999843537815 pt" VSHIFT_QUANTITY="11.249999664723878 pt"/>
+</node>
+</node>
+</map>
